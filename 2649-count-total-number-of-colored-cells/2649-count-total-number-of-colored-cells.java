6087549508885fd25fed090ed1f562720a6d64bb
Sathish <sathishvijay2003@gmail.com>
2025-03-05 10:37:11 +0530

    Time: 3 ms (42.21%), Space: 40.3 MB (73.36%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,5 +1,14 @@
 class Solution {
     public long coloredCells(int n) {
-        return (long)(n)*(n)+(long)(n-1)*(n-1);
+      //  return (long)(n)*(n)+(long)(n-1)*(n-1);
+        if(n==1){
+            return 1;
+        }
+        long k=4,m=1;
+        for(int i=1;i<n;i++){
+            m+=k;
+            k=k+4;
+        }
+        return m;
     }
 }
